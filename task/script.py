dce74fbd8c223c1df31f587a5047f30592ead179
root <root@server.localhost>
2021-06-29 16:30:42 -0400

    docker container

---
+++ task/script.py
@@ -0,0 +1,9 @@
+str1 = input("Please type first value:")
+str2 = input("Please type second value:")
+sorted_str1 = sorted(str1)
+sorted_str2 = sorted(str2)
+
+if sorted_str1 == sorted_str2:
+    print("True")
+else:
+    print("False")
